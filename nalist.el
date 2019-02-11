8a006ba2d8e045b2711c6b8aa5dac6d1b2e0b862
mukuge <cyriakus.h@gmail.com>
2019-02-12 00:10:52 +0900

    Changed the mode name from gcache to nalist.

--- nalist.el
+++ nalist.el
@@ -29,24 +29,24 @@
 
 (require 'cl-lib)
 
-(defmacro gcache-alist-clear (alist)
+(defmacro nalist-clear (alist)
   "Set ALIST nil."
   `(setq ,alist nil))
 
-(cl-defmacro gcache-alist-set (key value alist &key (testfn 'eq))
+(cl-defmacro nalist-set (key value alist &key (testfn 'eq))
   "Set a KEY VALUE pair in ALIST with TESTFN."
   `(setf (alist-get ,key ,alist nil nil ',testfn) ,value))
 
-(cl-defmacro gcache-alist-remove (key alist &key (testfn 'eq))
+(cl-defmacro nalist-remove (key alist &key (testfn 'eq))
   "Remove the pair with KEY in ALIST with TESTFN."
   `(setf (alist-get ,key ,alist nil t ',testfn) nil))
 
 
-(cl-defun gcache-alist-get (key alist &key default (testfn 'eq))
+(cl-defun nalist-get (key alist &key default (testfn 'eq))
   "Return the value of KEY in ALIST if exists TESTFN wise, otherwise DEFAULT."
   (alist-get key alist default nil testfn))
 
-(defun gcache-alist-subset-p (alist-a alist-b)
+(defun nalist-subset-p (alist-a alist-b)
   "Return t is ALIST-A is a sbuset of ALIST-B, otherwise nil."
   (let ((res t))
     (mapc #'(lambda (pair)
@@ -55,10 +55,10 @@
           alist-a)
     res))
 
-(defun gcache-alist-set-equal (alist-a alist-b)
+(defun nalist-set-equal (alist-a alist-b)
   "Return t if ALIST-A and ALIST-B are identical setwise, otherwise nil."
-  (and (gcache-alist-subset-p alist-a alist-b)
-       (gcache-alist-subset-p alist-b alist-a)))
+  (and (nalist-subset-p alist-a alist-b)
+       (nalist-subset-p alist-b alist-a)))
 
 
 (provide 'nalist)
