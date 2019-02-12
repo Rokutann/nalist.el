38c9fa2eb49311025abceb463abf2e213b5b5cc5
mukuge <cyriakus.h@gmail.com>
2019-02-12 21:48:55 +0900

    Noted about f.el.

--- bin/docs.el
+++ bin/docs.el
@@ -1,39 +1,42 @@
+;;; This script is based on docs.el in f.el.
+;;; See: https://github.com/rejeep/f.el/blob/master/bin/docs.el
+
 (require 'dash)
 (require 's)
 (require 'f)
 
-(defvar f-root-path
+(defvar nalist-root-path
   (expand-file-name ".." (file-name-directory load-file-name)))
 
-(defvar f-lib-file
-  (expand-file-name "nalist.el" f-root-path))
+(defvar nalist-lib-file
+  (expand-file-name "nalist.el" nalist-root-path))
 
-(defvar f-readme-file
-  (expand-file-name "README.md" f-root-path))
+(defvar nalist-readme-file
+  (expand-file-name "README.md" nalist-root-path))
 
-(defvar f-readme-template
-  (expand-file-name "README.md.tpl" f-root-path))
+(defvar nalist-readme-template
+  (expand-file-name "README.md.tpl" nalist-root-path))
 
-(defvar f-fn-doc-mapping (make-hash-table :test 'equal))
+(defvar nalist-fn-doc-mapping (make-hash-table :test 'equal))
 
-(require 'nalist f-lib-file)
+(require 'nalist nalist-lib-file)
 
 (-map
  (lambda (lib)
-   (when (equal (car lib) f-lib-file)
+   (when (equal (car lib) nalist-lib-file)
      (-select
       (lambda (alist)
         (when (and
                (listp alist)
                (equal (car alist) 'defun)
                (s-matches? "^nalist-[^-][a-z-]+\\??$" (symbol-name (cdr alist))))
-          (puthash (symbol-name (cdr alist)) (documentation (cdr alist)) f-fn-doc-mapping)))
+          (puthash (symbol-name (cdr alist)) (documentation (cdr alist)) nalist-fn-doc-mapping)))
       (cdr lib))))
  load-history)
 
-(let ((content (f-read f-readme-template)))
+(let ((content (f-read nalist-readme-template)))
   (maphash
    (lambda (fn doc)
      (setq content (s-replace (concat "{{" fn "}}") doc content)))
-   f-fn-doc-mapping)
-  (f-write content 'utf-8 f-readme-file))
+   nalist-fn-doc-mapping)
+  (f-write content 'utf-8 nalist-readme-file))
