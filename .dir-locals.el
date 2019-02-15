a8803b00e4fd5a553b4480130f01eb7e42e6b7b6
mukuge <cyriakus.h@gmail.com>
2019-02-15 23:50:07 +0900

    Changed to a make workflow.

--- .dir-locals.el
+++
@@ -1,9 +0,0 @@
-((nil . ((eval . (progn
-                   (require 'projectile)
-                   (puthash (projectile-project-root)
-                            "cask exec ert-runner"
-                            projectile-test-cmd-map))))))
-
-;; Local Variables:
-;; flycheck-disabled-checkers: (emacs-lisp-checkdoc)
-;; End:
